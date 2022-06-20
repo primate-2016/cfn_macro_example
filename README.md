ee3de9317dfec7916c1e9da0bd657b7ccd95c82d
Richard <moo@elasticcows.co.uk>
2022-06-20 16:37:33 +0100

    tweaks

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cfn_macro_example
-Unecessary CFN Macro example
+Unecessary CFN Macro example so that I don't forget how they work again.....
+
+aws cloudformation package --template-file ./template.yml --s3-bucket *your_bucket_name* --output-template-file output.yaml
+
+aws cloudformation deploy --template-file ./output.yaml --stack-name *stack_name* --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
--- src/macro_output_ssm_params/macro_output_ssm_params.py
+++ src/macro_output_ssm_params/macro_output_ssm_params.py
@@ -15,10 +15,8 @@ def handler(event, context):
     logger.info(f'Going to output {number_of_ssm_params} SSM parameters')
 
     # we will output some full CFN resources in a loop here
-    n = 1
-    for param in int(number_of_ssm_params):
-        event['fragment']['Resources'][f'SsmParam{n}'] = json.dumps(
-            {
+    for n in range(int(number_of_ssm_params)):
+        event['fragment']['Resources'][f'SsmParam{n}'] = {
             'Type' : 'AWS::SSM::Parameter',
             'Properties' : {
                 'Description' : f'This is the SSM param {n}',
@@ -27,13 +25,11 @@ def handler(event, context):
                 'Value' : 'something exciting'
                 }
             }
-        )
-
-        n += 1
 
     # we can also update a specific bit of a resource in a template using a macro
     event['fragment']['Resources']['NormalSsmParam']['Properties']['Value'] = 'This was changed by the CFN Macro'
 
+    logger.info(f'returning the following to CFN: {json.dumps(event)}')
 
     return {
         'requestId': event['requestId'],
